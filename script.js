d795f89d4ce26d00da69bdbf997a29e5500b77e7
Pronothurah <onsongojosphat28@gmail.com>
2023-08-02 12:46:39 +0300

    UI logic

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 
-function game() {
+document.addEventListener("DOMContentLoaded", function() {
+    let rockBtn = document.getElementById("rock");
+    let paperBtn = document.getElementById("paper");
+    let scissorsBtn = document.getElementById("scissors");
+    let playerScoreElement = document.getElementById("playerScore");
+    let computerScoreElement = document.getElementById("computerScore");
+    let outcomeElement = document.getElementById('outcome');
+
+    let playerScore = 0;
+    let computerScore = 0;
 
     function playRound(playerSelection, computerSelection) {
-
+    
         playerSelection = playerSelection.toLowerCase();
         computerSelection = computerSelection.toLowerCase();
     
@@ -29,34 +38,44 @@ function game() {
         return choices[randomIndex];
     };
 
-    let playerScore = 0;
-    let computerScore = 0;
 
-    for (let round = 1; round <= 5; round++) {
-        let playerSelection = prompt("Please enter your input for round" + round + ':');
-        let computerSelection = getComputerChoice();
-        let playResult = playRound(playerSelection, computerSelection);
+    function updateDisplay() {
+        playerScoreElement.textContent = playerScore;
+        computerScoreElement.textContent = computerScore;
+    };
 
-        console.log('Result for round ' + round + ':' + playResult);
-
+    function updateScores(playResult) {
         if(playResult.startsWith('You Win')) {
             playerScore ++;
         } else if (playResult.startsWith('You Lose')) {
             computerScore ++;
         } 
+
+        updateDisplay();
+        outcomeElement.textContent = playResult;
     }
 
-    if (playerScore > computerScore) {
-        return 'You Won the game';
-    } else if (computerScore > playerScore) {
-        return 'You Lost the game';
-    } else {
-        return 'Its a tie';
+    function determineOutcome() {
+        if (playerScore > computerScore) {
+            outcomeElement.textContent = 'You Won the game';
+        } else if (computerScore > playerScore) {
+            outcomeElement.textContent = 'You Lost the game';
+        } else {
+            outcomeElement.textContent ='Its a tie';
+        }
     }
 
-}
+    function game(playerSelection) {
 
+        const computerSelection = getComputerChoice();
+        const playResult = playRound(playerSelection, computerSelection);
+        updateScores(playResult);
+        determineOutcome();
+    }
 
-const play = game();
+    
+    rockBtn.addEventListener('click', () => game('rock'));
+    paperBtn.addEventListener('click', () => game('paper'));
+    scissorsBtn.addEventListener('click', () => game('scissors'));
+});
 
-console.log(play);
